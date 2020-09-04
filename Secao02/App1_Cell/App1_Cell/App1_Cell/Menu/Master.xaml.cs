2f3486abb37d443fd3117e9d762cfa32668d7d34
Marco Aurelio <marco.barros@patrus.com.br>
2020-09-04 16:03:59 -0300

    Criando o Projeto

---
+++ Secao02/App1_Cell/App1_Cell/App1_Cell/Menu/Master.xaml.cs
@@ -0,0 +1,31 @@
+﻿using App1_Cell.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App1_Cell.Menu
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Master : MasterDetailPage
+    {
+        public Master()
+        {
+            InitializeComponent();
+        }
+
+        public  void GoPagina1(object sender, EventArgs arg)
+        {
+            Detail = new NavigationPage(new TextCellPage());
+        }
+
+        public void GoPagina2(object sender, EventArgs arg)
+        {
+            Detail = new NavigationPage(new ImageCeallPage());
+        }
+    }
+}
---
+++ Secao02/App1_Cell/App1_Cell/App1_Cell/Pages/ImageCeallPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using App1_Cell.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App1_Cell.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ImageCeallPage : ContentPage
+    {
+        public ImageCeallPage()
+        {
+            InitializeComponent();
+
+            List<Funcionario> Lista = new List<Funcionario>();
+            Lista.Add(new Funcionario() { Nome = "José", Cargo = "Presidente da Empresa" , Foto= "http://helpsaw.trixti.com.br/helpsaw_administrador/Administrador/business_user.png" });
+            Lista.Add(new Funcionario() { Nome = "Maria", Cargo = "Gerente de Vendas", Foto = "http://helpsaw.trixti.com.br/helpsaw_administrador/Administrador/business_user.png" });
+            Lista.Add(new Funcionario() { Nome = "Elaine", Cargo = "Gerente de Marketing",  Foto = "http://helpsaw.trixti.com.br/helpsaw_administrador/Administrador/business_user.png" });
+            Lista.Add(new Funcionario() { Nome = "Felipe", Cargo = "Entregador", Foto = "http://helpsaw.trixti.com.br/helpsaw_administrador/Administrador/business_user.png" });
+            Lista.Add(new Funcionario() { Nome = "João", Cargo = "Vendedor", Foto = "http://helpsaw.trixti.com.br/helpsaw_administrador/Administrador/business_user.png" });
+
+
+            ListaFuncionario.ItemsSource = Lista;
+        }
+
+    }
+}

914df9dc1d59f4dc379b2e69b2c27df9da82ea97
Marco Aurelio <marco.barros@patrus.com.br>
2020-09-14 12:11:14 -0300

    todos Styles

---
+++ Secao03/App1_Estilo/Estilo/Estilo/Estilo/Master/Master.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Estilo.Master
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Master : MasterDetailPage
+    {
+        public Master()
+        {
+            InitializeComponent();
+        }
+
+        private void GoPagina1(object sender, EventArgs args)
+        {
+            Detail = new NavigationPage(new Paginas.ImplicitStylePage());
+        }
+        private void GoPagina2(object sender, EventArgs args)
+        {
+            Detail = new NavigationPage(new Paginas.ExplicitStylePage());
+        }
+        private void GoPagina3(object sender, EventArgs args)
+        {
+            Detail = new NavigationPage(new Paginas.GlobalStylePage());
+        }
+        private void GoPagina4(object sender, EventArgs args)
+        {
+            Detail = new NavigationPage(new Paginas.InheritStylePage());
+        }
+        private void GoPagina5(object sender, EventArgs args)
+        {
+            Detail = new NavigationPage(new Paginas.DynamicStylePage());
+        }
+    }
+}
